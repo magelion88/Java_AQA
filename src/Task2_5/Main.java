551aa08c63745c9f3f9c14f5ed72e2b0a9a4f961
magelion88 <119241929+magelion88@users.noreply.github.com>
2025-12-24 12:31:27 +0300

    Add MyArrayDataException

--- src/Task2_5/Main.java
+++ src/Task2_5/Main.java
@@ -4,17 +4,49 @@ class MyArraySizeException extends Exception {
     MyArraySizeException(String s) { super(s); }
 }
 
+class MyArrayDataException extends Exception {
+    MyArrayDataException(int i, int j, String value) {
+        super("Ошибка в [" + i + "][" + j + "]: '" + value + "' - не число");
+    }
+}
 public class Main {
-    public static void checkArray(String[][] a) throws MyArraySizeException {
+    public static int checkArray(String[][] a) throws MyArraySizeException, MyArrayDataException {
         if (a.length != 4) throw new MyArraySizeException("Не 4 строки");
         for (String[] row : a) {
             if (row.length != 4) throw new MyArraySizeException("Не 4 столбца");
         }
+        int sum = 0;
+
+        for(int i = 0; i < 4; i++) {
+           for(int j = 0; j < 4; j++) {
+               try {
+                   sum += Integer.parseInt(a[i][j]);
+               }
+               catch(NumberFormatException e) {
+                   throw new MyArrayDataException(i, j, a[i][j]);
+               }
+           }
+        }
+        return sum;
     }
 
     public static void main(String[] args) throws MyArraySizeException {
-        String[][] test = new String[4][4];
-        checkArray(test);
-        System.out.println("OK");
+        String[][] testArray = {
+                {"10", "20", "30", "40"},
+                {"50", "60", "70", "80"},
+                {"90", "100", "110", "не число"},
+                {"130", "140", "150", "160"}
+    };
+    try {
+        int result = checkArray(testArray);
+        // Если успешно - выводим сумму
+        System.out.println("Сумма: " + result);
+    } catch (MyArraySizeException e) {
+        // Ловим исключение неправильного размера
+        System.out.println("Ошибка размера: " + e.getMessage());
+    } catch (MyArrayDataException e) {
+        // Ловим исключение неправильных данных
+        System.out.println("Ошибка данных: " + e.getMessage());
+    }
     }
 }
